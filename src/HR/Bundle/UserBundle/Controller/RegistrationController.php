0298cf772600f373a940625812c3df582c2370cf
Wenming Tang <tang@babyfamily.com>
2013-10-01 22:26:34 +0800

    add registration controller

---
+++ src/HR/Bundle/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,16 @@
+<?php
+namespace HR\Bundle\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class RegistrationController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function registerAction()
+    {
+        return array();
+    }
+}
