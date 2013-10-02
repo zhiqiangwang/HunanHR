ce021490f7db6cc77ae8fc2b55973a4f71917ba7
Wenming Tang <tang@babyfamily.com>
2013-10-02 23:02:41 +0800

    implement register

--- src/HR/Bundle/UserBundle/Controller/RegistrationController.php
+++ src/HR/Bundle/UserBundle/Controller/RegistrationController.php
@@ -2,6 +2,8 @@
 namespace HR\Bundle\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class RegistrationController extends Controller
@@ -9,8 +11,26 @@ class RegistrationController extends Controller
     /**
      * @Template()
      */
-    public function registerAction()
+    public function registerAction(Request $request)
     {
-        return array();
+        /** @var \Symfony\Component\Form\FormInterface $form */
+        $form = $this->get('user.form.registration');
+
+        /** @var \HR\Bundle\UserBundle\EntityManager\UserManager $userManager */
+        $userManager = $this->get('user.manager');
+        $user        = $userManager->createUser();
+
+        $form->setData($user);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $userManager->updateUser($user);
+
+            return $this->redirect($this->generateUrl('home'));
+        }
+
+        return array(
+            'form' => $form->createView()
+        );
     }
 }
---
+++ src/HR/Bundle/UserBundle/FormType/RegistrationFormType.php
@@ -0,0 +1,40 @@
+<?php
+namespace HR\Bundle\UserBundle\FormType;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+/**
+ * @author Wenming Tang <tang@babyfamily.com>
+ */
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('screenName', 'text', array('label' => '昵称'))
+            ->add('email', 'email', array('label' => '电子邮件地址'))
+            ->add('username', 'text', array('label' => '用户名'))
+            ->add('plainPassword', 'repeated', array(
+                'type'            => 'password',
+                'first_options'   => array('label' => '密码'),
+                'second_options'  => array('label' => '重新输入密码'),
+                'invalid_message' => '两次输入密码不匹配'
+            ))
+            ->add('save', 'submit', array('label' => '注册'));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class'        => 'HR\Bundle\UserBundle\Entity\User',
+            'validation_groups' => array('registration')
+        ));
+    }
+
+    public function getName()
+    {
+        return 'registration';
+    }
+}
