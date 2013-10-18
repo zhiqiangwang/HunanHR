966935d294fe4986b60244019601d1b84b9d15ea
Wenming Tang <tang@babyfamily.com>
2013-10-18 17:07:15 +0800

    qq connect

--- src/HR/OAuthBundle/Controller/ConnectController.php
+++ src/HR/OAuthBundle/Controller/ConnectController.php
@@ -152,6 +152,22 @@ class ConnectController extends Controller
         $user->setBio($userInformation->getBio());
         $user->setHomepage($userInformation->getHomepage());
 
+        if ($userInformation->getResourceOwner()->getName() == 'qq') {
+
+            $user->setUsername($userInformation->getNickname());
+            $user->setPassword('');
+            $user->setEmail('');
+
+            $this->container->get('hwi_oauth.account.connector')->connect($user, $userInformation);
+            $this->setFlash('注册完成，请完善您的资料。');
+
+            $response = $this->redirect($this->generateUrl('profile_edit'));
+
+            $dispatcher->dispatch(UserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+            return $response;
+        }
+
         $form->setData($user);
         $form->handleRequest($request);
 
