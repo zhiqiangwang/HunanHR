af44f5992ef68572cd4ebe88f2430ebbbbd4afd3
Wenming Tang <tang@babyfamily.com>
2013-10-21 21:58:53 +0800

    substring

--- src/HR/PositionBundle/Twig/Extension/StringExtension.php
+++ src/HR/PositionBundle/Twig/Extension/StringExtension.php
@@ -15,6 +15,10 @@ class StringExtension extends \Twig_Extension
 
     public function substring($input, $length)
     {
+        if (false !== mb_stripos($input, '[tag]')) {
+            return self::tagSubstring($input, $length);
+        }
+
         $len       = mb_strlen($input);
         $substring = mb_substr($input, 0, $length);
 
@@ -24,6 +28,7 @@ class StringExtension extends \Twig_Extension
 
         return $substring;
     }
+
 
     public function obfuscateEmail($email)
     {
@@ -37,5 +42,47 @@ class StringExtension extends \Twig_Extension
     public function getName()
     {
         return 'markup_extension.substring';
+    }
+
+    private static function tagSubstring($input, $length)
+    {
+        $splitLines = preg_split('/(\[tag\]|\[\/tag\])/u', $input, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+
+        $inTag    = false;
+        $position = 0;
+        $tagLen   = 0;
+        foreach ($splitLines as $line) {
+            if ($line == '[tag]' || $line == '[/tag]') {
+                if ($line == '[tag]') {
+                    $inTag = true;
+                }
+
+                if ($line == '[/tag]') {
+                    $inTag = false;
+                }
+
+                $tagLen += strlen($line);
+                continue;
+            }
+
+            $position += mb_strlen($line);
+
+            if ($position >= $length) {
+                $position -= ($position - $length);
+                break;
+            }
+        }
+
+        $substring = mb_substr($input, 0, $position + $tagLen);
+
+        if ($inTag) {
+            $substring .= '[/tag]';
+        }
+
+        if ($position >= $length) {
+            $substring .= '...';
+        }
+
+        return $substring;
     }
 }
